f4bafd88c5c91d9eadd38b73811cfa5a2c2c45d8
Kukumberchick <durovalexei93@gmail.com>
2024-03-01 17:43:35 +0600

    add home swiper

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./components/GlobalStyle/style.css"
+import HomeSwiper from "./components/pages/HomeSwiper/HomeSwiper";
+
+function App() {
+  return <div className="App">
+    <HomeSwiper />
+  </div>;
+}
+
+export default App;
