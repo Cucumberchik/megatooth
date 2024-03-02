89dd0b6e39dad0bc2e61ee4a3ca7006fe02fc9d6
Kukumberchick <durovalexei93@gmail.com>
2024-03-02 11:11:39 +0600

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./components/GlobalStyle/style.css"
 import "./components/GlobalStyle/Media.css"
-import { AboutUsOurDoctor } from "./components/pages/Home/AboutUsOurDoctor";
+
 
 function App() {
   return <div className="App">
-    <AboutUsOurDoctor />
   </div>;
 }
 
