feda7ac6e5ac161544d0f1dd067aa262a8ce2ed0
Kukumberchick <durovalexei93@gmail.com>
2024-03-02 11:10:38 +0600

    add adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./components/GlobalStyle/style.css"
+import "./components/GlobalStyle/Media.css"
+import { AboutUsOurDoctor } from "./components/pages/Home/AboutUsOurDoctor";
 
 function App() {
   return <div className="App">
-
+    <AboutUsOurDoctor />
   </div>;
 }
 
