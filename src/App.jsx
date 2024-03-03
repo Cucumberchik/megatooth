0480095770a7331e6ed47c69ac94d3442c60acbe
manndar1n <161709694+manndar1n@users.noreply.github.com>
2024-03-03 18:41:04 +0600

    service

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import "./components/GlobalStyle/style.css"
-import "./components/GlobalStyle/Media.css"
+import "./components/GlobalStyle/style.css";
+import "./components/GlobalStyle/Media.css";
 import Navbar from "./components/pages/Navbar/Navbar";
-import Routers from "./components/Routes/Routes"
+import Routers from "./components/Routes/Routes";
 
 function App() {
-  return <div className="App">
-    <Navbar />
-    <Routers />
-  </div>;
+    return (
+        <div className="App">
+            <Navbar />
+            <Routers />
+        </div>
+    );
 }
 
 export default App;
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { HomeSwiper } from './HomeSwiper'
-import { AboutUsOurDoctor } from './AboutUsOurDoctor'
+import React from "react";
+import { HomeSwiper } from "./HomeSwiper";
+import { AboutUsOurDoctor } from "./AboutUsOurDoctor";
+import Main from "../Hero/main";
 
 export default function Home() {
     return (
         <div>
             <HomeSwiper />
+            <Main />
             <AboutUsOurDoctor />
         </div>
-    )
+    );
 }
