2448d8fbe8b7c608fda52f85ae69b3b77bc6912a
manndar1n <161709694+manndar1n@users.noreply.github.com>
2024-03-05 01:21:26 +0600

    finished social

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { HomeSwiper } from "./HomeSwiper";
 import { AboutUsOurDoctor } from "./AboutUsOurDoctor";
 import Main from "../Hero/main";
+import Social from "../Social/Social";
 
 export default function Home() {
     return (
         <div>
             <HomeSwiper />
             <Main />
+            <Social />
             <AboutUsOurDoctor />
         </div>
     );
