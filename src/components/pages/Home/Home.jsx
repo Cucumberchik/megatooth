c0dfd7bd75a90b22c042c9e3fac02fc6135f9277
manndar1n <161709694+manndar1n@users.noreply.github.com>
2024-03-06 19:26:52 +0600

    finished news
    News

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { HomeSwiper } from './HomeSwiper'
-import { AboutUsOurDoctor } from './AboutUsOurDoctor'
-import Main from "../Hero/main";
-import HomeRating from "./HomeRating"
+import React from "react";
+import { HomeSwiper } from "./HomeSwiper";
+import { AboutUsOurDoctor } from "./AboutUsOurDoctor";
+import HomeRating from "./HomeRating";
 import Social from "../Social/Social";
+import News from "../News/News";
 
 export default function Home() {
     return (
         <div>
             <HomeSwiper />
-            <Main />
             <Social />
+            <News />
             <AboutUsOurDoctor />
             <HomeRating />
         </div>
